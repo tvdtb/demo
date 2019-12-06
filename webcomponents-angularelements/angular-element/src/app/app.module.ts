547b037d13799661522bbd82fe997ba629843fba
Marius <Django987@users.noreply.github.com>
2019-12-06 10:59:01 +0100

    create Angular Element

--- webcomponents-angularelements/angular-element/src/app/app.module.ts
+++ webcomponents-angularelements/angular-element/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
@@ -13,6 +14,16 @@ import { CustomButtonComponent } from './custom-button/custom-button.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [CustomButtonComponent], //expliziter Aufruf an Angular Komponente inital zu laden
+  // bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const ngElement = createCustomElement(CustomButtonComponent, {
+      injector: this.injector
+    });
+    customElements.define('my-button', ngElement);
+  }
+}
