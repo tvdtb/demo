72d3cd964f7b78aa372b7896163fc1983f0812d1
Marius <Django987@users.noreply.github.com>
2019-12-14 16:59:29 +0100

    delete initial app.component

--- webcomponents-angularelements/angular-element/src/app/app.module.ts
+++ webcomponents-angularelements/angular-element/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
 import { customElementPlease } from './webcomponent-creating-process';
 
 @NgModule({
   declarations: [
-    AppComponent,
     CustomButtonComponent
   ],
   imports: [
@@ -26,8 +24,8 @@ export class AppModule {
       injector: this.injector
     });
 
-    const newNgElement = customElementPlease(CustomButtonComponent,  this.injector)
-    // customElements.define('my-button', ngElement);
-    customElements.define('my-button', newNgElement);
+    // const newNgElement = customElementPlease(CustomButtonComponent,  this.injector)
+    customElements.define('my-button', ngElement);
+    // customElements.define('my-button', newNgElement);
   }
 }
